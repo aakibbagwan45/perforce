373580337243f75ee3e268b286adbaa5a8101b82
Aakibbagwan45 <51746890+aakibbagwan45@users.noreply.github.com>
2022-10-26 19:31:54 +0530

    Delete terraform.tf

--- terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-   bucket = "myjenkinslab-terraform"
-   region = "ap-south-1"
-   shared_credentials_file = "/aws/cred"
-   profile               = "awscred"
-   dynamodb_table = "state-locking"
-  }
-}
